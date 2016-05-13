fa21baf36365a46d13571c8fe427717d285ade12
shekhar madnawat <shekhar.madnawat@mobiloittegroup.com>
2016-05-13 12:00:05 +0530

    push test code for gem

---
+++ egg_roll/lib/egg_roll.rb
@@ -0,0 +1,8 @@
+require "egg_roll/version"
+
+module EggRoll
+  # Your code goes here...
+  def use_egg
+  	p "%%%%%egg_used%%%%%"
+  end
+end
---
+++ egg_roll/lib/egg_roll/version.rb
@@ -0,0 +1,3 @@
+module EggRoll
+  VERSION = "1.0.0"
+end
